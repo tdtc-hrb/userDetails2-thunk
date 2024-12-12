021622e9b20a10f72cfc0d03f0be96a342621d38
tdtc-hrb <tdtc.hrb@gmail.com>
2024-12-12 05:16:01 -0800

    Initial project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk'; // v3
+// import thunk from 'redux-thunk'; // v2
+import { createLogger } from 'redux-logger';
+import reducer from './reducers';
+
+const middleware = [ thunk ];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Provider store={store}>
+        <App />
+    </Provider>);
+
+registerServiceWorker();
